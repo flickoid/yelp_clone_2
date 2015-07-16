337cf99dd17b53d8163a28a28ceabb07d4c0aecf
flickoid <chris.oatley@gmail.com>
2015-07-16 02:04:19 +0100

    Added functionality for editing individual restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 feature 'restaurants' do
 
   context 'no restaurants have been added' do
-
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
@@ -15,7 +14,6 @@ feature 'restaurants' do
     before do
       Restaurant.create(name: 'KFC')
     end
-
     scenario 'display restaurants' do
       visit '/restaurants'
       expect(page).to have_content('KFC')
@@ -35,9 +33,7 @@ feature 'restaurants' do
   end
 
   context 'viewing restaurants' do
-
     let!(:kfc){Restaurant.create(name: 'KFC')}
-
     scenario 'lets a user view a restaurant' do
       visit '/restaurants'
       click_link 'KFC'
@@ -46,4 +42,38 @@ feature 'restaurants' do
     end
   end
 
+  context 'editing restaurants' do
+    before {Restaurant.create name: 'KFC'}
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(page).not_to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
